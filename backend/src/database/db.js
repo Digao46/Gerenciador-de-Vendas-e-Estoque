d1564cb77975338bb8c96d99a13da1af72fc1eb2
Diogo Melo <98532975+Digao46@users.noreply.github.com>
2022-11-18 00:49:29 -0300

    .

---
+++ backend/src/database/db.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.db = void 0;
+const sequelize_1 = require("sequelize");
+const dbName = process.env.DATABASE_NAME;
+const dbUser = process.env.DATABASE_USER;
+const dbPassword = process.env.DATABASE_PASSWORD;
+const dbHost = process.env.DATABASE_HOST;
+const dbPort = process.env.DATABASE_PORT;
+exports.db = new sequelize_1.Sequelize(dbName, dbUser, dbPassword, {
+    dialect: "mysql",
+    host: dbHost,
+    port: +dbPort,
+    timezone: "-03:00",
+});

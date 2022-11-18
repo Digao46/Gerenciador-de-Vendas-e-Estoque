c93c0468913e068e89bcac9f8cda9577ffe7c982
Diogo Melo <98532975+Digao46@users.noreply.github.com>
2022-11-18 11:38:12 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: "../.env"});
+dotenv.config({ path: __dirname + "/../.env" });
 
 import express, { json } from "express";
 import { db } from "./database/db";

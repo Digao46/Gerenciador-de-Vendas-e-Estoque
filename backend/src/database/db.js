2441fd44f14feda30d5ddfce1bb46ca29c4658b2
Diogo Melo <98532975+Digao46@users.noreply.github.com>
2022-11-17 23:32:36 -0300

    .

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -2,15 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 const sequelize_1 = require("sequelize");
-require('dotenv').config();
+require("dotenv").config();
 const dbName = process.env.DATABASE_NAME;
 const dbUser = process.env.DATABASE_USER;
 const dbPassword = process.env.DATABASE_PASSWORD;
 const dbHost = process.env.DATABASE_HOST;
 const dbPort = process.env.DATABASE_PORT;
-exports.db = new sequelize_1.Sequelize(dbName, dbUser, dbPassword, {
+exports.db = new sequelize_1.Sequelize("sales", "root", "59772136Dig@o", {
     dialect: "mysql",
-    host: dbHost,
-    port: +dbPort,
+    host: "localhost",
+    port: 3306,
     timezone: "-03:00",
 });
--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const dbName = process.env.DATABASE_NAME!;
 const dbUser = process.env.DATABASE_USER!;
@@ -8,9 +8,9 @@ const dbPassword = process.env.DATABASE_PASSWORD!;
 const dbHost = process.env.DATABASE_HOST!;
 const dbPort = process.env.DATABASE_PORT!;
 
-export const db = new Sequelize(dbName, dbUser, dbPassword, {
+export const db = new Sequelize("sales", "root", "59772136Dig@o", {
   dialect: "mysql",
-  host: dbHost,
-  port: +dbPort,
+  host: "localhost",
+  port: 3306,
   timezone: "-03:00",
 });
